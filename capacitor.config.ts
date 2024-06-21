070772fa873285b086015386e7608e5342760649
UldrenWEB <uldrenmiguel33@gmail.com>
2024-06-20 23:15:28 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'Spyral Music',
+  webDir: 'www'
+};
+
+export default config;
