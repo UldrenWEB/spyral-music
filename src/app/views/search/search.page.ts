2219753a85435417448e126a3a0b84bdbc96b21f
UldrenWEB <uldrenmiguel33@gmail.com>
2024-06-25 04:12:53 -0400

    vista [search-page]: Se agrego el buscador y el select, a demas se agrego la logica correcta para buscar

--- src/app/views/search/search.page.ts
+++ src/app/views/search/search.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { LoadingController } from '@ionic/angular';
 
 @Component({
   selector: 'app-search',
@@ -6,7 +7,45 @@ import { Component } from '@angular/core';
   styleUrls: ['search.page.scss']
 })
 export class SearchPage {
+  
+  searchTxt: string = '';
+  selectedChange: string = 'song';
+  isLoading: boolean = false;
+  private searchTimeout: any = null;
 
-  constructor() {}
+  constructor(private loaderController: LoadingController) {}
+
+
+  onChange(event: Event) {
+    const value = (event.target as HTMLInputElement).value;
+    this.searchTxt = value;
+
+    if (this.searchTimeout) {
+      clearTimeout(this.searchTimeout);
+    }
+
+    this.searchTimeout = setTimeout(() => {
+      this.performSearch();
+    }, 800);
+  }
+
+  //Este busca luego de que el usuario escribio
+  async performSearch() {
+
+    this.isLoading = true;
+    const loading = await this.loaderController.create({
+      message: 'Buscando...',
+      spinner: 'circular'
+    });
+
+    await loading.present();
+
+    setTimeout(async () => {
+      this.isLoading = false;
+      await loading.dismiss();
+    }, 3000)
+    console.log('Buscando:', this.searchTxt);
+    console.log('Por: ', this.selectedChange);
+  }
 
 }
